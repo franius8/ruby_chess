0cd2c4303750019f2cb1b6da83831d8177ffec5f
franius8 <skawinskifranciszek@gmail.com>
2022-10-13 09:08:11 +0200

    Create piece adding methods

--- lib/ruby_chess.rb
+++ lib/ruby_chess.rb
@@ -29,7 +29,11 @@ module BoardPrinting
         @board.each_index do |row|
             print " #{number} |"
             @board[row].each_index do |space|
-                print " #{@board[row][space]} |"
+                if board[row][space] == '*'
+                    print " #{@board[row][space]} |"
+                else
+                    print " #{@board[row][space].symbol} |"
+                end
             end
             print " #{number}"
             print "\n"
@@ -45,6 +49,40 @@ class Board
     def initialize
         @board = Array.new(8, '*')
         @board.each_index {|index| @board[index] = Array.new(8, '*')}
+        add_pieces
+    end
+
+    def add_pieces
+        add_black_pieces
+        add_white_pieces
+    end
+
+    def add_white_pieces
+        @board[0][0] = Piece.new('rook', 'black', [0, 0])
+        @board[0][7] = Piece.new('rook', 'black', [0, 7])
+        @board[0][1] = Piece.new('knight', 'black', [0, 1])
+        @board[0][6] = Piece.new('knight', 'black', [0, 6])
+        @board[0][2] = Piece.new('bishop', 'black', [0, 2])
+        @board[0][5] = Piece.new('bishop', 'black', [0, 5])
+        @board[0][3] = Piece.new('queen', 'black', [0, 3])
+        @board[0][4] = Piece.new('king', 'black', [0, 4])
+        @board[1].each_index do |index|
+            @board[1][index] = Piece.new('pawn', 'black', [1, index])
+        end
+    end
+
+    def add_black_pieces
+        @board[7][0] = Piece.new('rook', 'white', [7, 0])
+        @board[7][7] = Piece.new('rook', 'white', [7, 7])
+        @board[7][1] = Piece.new('knight', 'white', [7, 1])
+        @board[7][6] = Piece.new('knight', 'white', [7, 6])
+        @board[7][2] = Piece.new('bishop', 'white', [7, 2])
+        @board[7][5] = Piece.new('bishop', 'white', [7, 5])
+        @board[7][3] = Piece.new('queen', 'white', [7, 3])
+        @board[7][4] = Piece.new('king', 'white', [7, 4])
+        @board[6].each_index do |index|
+            @board[6][index] = Piece.new('pawn', 'white', [6, index])
+        end
     end
 end
 
