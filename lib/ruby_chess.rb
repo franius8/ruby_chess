9b33bcab67ad2a4fa200eb58f8e36a97f3d04a9a
franius8 <skawinskifranciszek@gmail.com>
2022-10-15 17:52:17 +0200

    Move symbol assigner to separate file

--- lib/ruby_chess.rb
+++ lib/ruby_chess.rb
@@ -4,43 +4,7 @@ require 'pry-byebug'
 require 'colorize'
 
 require_relative 'board_printing'
-
-# Assigns symbol to individual chess pieces
-module SymbolAssigner
-  def assign_white_symbol
-    case @type
-    when 'pawn'
-      @symbol = "\u2659"
-    when 'knight'
-      @symbol = "\u2658"
-    when 'bishop'
-      @symbol = "\u2657"
-    when 'rook'
-      @symbol = "\u2656"
-    when 'king'
-      @symbol = "\u2654"
-    when 'queen'
-      @symbol = "\u2655"
-    end
-  end
-
-  def assign_black_symbol
-    case @type
-    when 'pawn'
-      @symbol = "\u265F"
-    when 'knight'
-      @symbol = "\u265E"
-    when 'bishop'
-      @symbol = "\u265D"
-    when 'rook'
-      @symbol = "\u265C"
-    when 'king'
-      @symbol = "\u265A"
-    when 'queen'
-      @symbol = "\u265B"
-    end
-  end
-end
+require_relative 'symbol_assigner'
 
 # Creates the arrays of posible moves used by chess pieces
 module PossibleMovesCreator
---
+++ lib/symbol_assigner.rb
@@ -0,0 +1,36 @@
+# Assigns symbol to individual chess pieces
+module SymbolAssigner
+    def assign_white_symbol
+      case @type
+      when 'pawn'
+        @symbol = "\u2659"
+      when 'knight'
+        @symbol = "\u2658"
+      when 'bishop'
+        @symbol = "\u2657"
+      when 'rook'
+        @symbol = "\u2656"
+      when 'king'
+        @symbol = "\u2654"
+      when 'queen'
+        @symbol = "\u2655"
+      end
+    end
+  
+    def assign_black_symbol
+      case @type
+      when 'pawn'
+        @symbol = "\u265F"
+      when 'knight'
+        @symbol = "\u265E"
+      when 'bishop'
+        @symbol = "\u265D"
+      when 'rook'
+        @symbol = "\u265C"
+      when 'king'
+        @symbol = "\u265A"
+      when 'queen'
+        @symbol = "\u265B"
+      end
+    end
+  end
